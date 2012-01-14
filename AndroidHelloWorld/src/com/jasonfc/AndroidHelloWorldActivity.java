508452102a10dcb4bdde7d43be435dc8f60e9ad4
Jason Fung <jasonfungsing@gmail.com>
2012-01-14 21:26:56 +1100

    Added layout, added show web view

--- AndroidHelloWorld/src/com/jasonfc/AndroidHelloWorldActivity.java
+++ AndroidHelloWorld/src/com/jasonfc/AndroidHelloWorldActivity.java
@@ -1,13 +1,55 @@
 package com.jasonfc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AndroidHelloWorldActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	private EditText text;
+	private Uri url;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		text = (EditText) findViewById(R.id.editText1);
+	}
+
+	public void onClickShowWebView(View view) {
+		if (isInputValid()) {
+			Intent i = new Intent(Intent.ACTION_VIEW, url);
+			startActivity(i);
+		}
+	}
+
+	public void onClickShowSourceWeb(View view) {
+		isInputValid();
+
+	}
+
+	private boolean isInputValid() {
+		boolean isInputValid = false;
+		String uri = text.getText().toString();
+		if (uri.length() == 0) {
+			Toast.makeText(this, "God, at least enter something", Toast.LENGTH_SHORT).show();
+		} else if (uri.startsWith("http://") || uri.startsWith("https://")) {
+			url = Uri.parse(uri);
+			isInputValid = true;
+		} else {
+			uri = "http://" + uri;
+			url = Uri.parse(uri);
+			isInputValid = true;
+		}
+
+		return isInputValid;
+	}
+	
+	
+	
 }
